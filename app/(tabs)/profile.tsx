bf2b46ad76b8c0373cf1e4cde3dd86b9ef542e1b
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-07-06 20:02:35 +0900

    feat: 페이지 별 임시 내용 수정 및 삽입

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -4,7 +4,7 @@ import { ThemedView } from '@/components/ThemedView';
 export default function ProfileScreen() {
   return (
     <ThemedView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <ThemedText type="title">홈</ThemedText>
+      <ThemedText type="title">프로필</ThemedText>
     </ThemedView>
   );
 }
