00ed752e3e6398c48e22b5c85614e1bdb01fd40b
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-07-11 01:06:11 +0900

    style: 기본 페이지 디자인 적용

---
+++ components/ui/StyledScreen.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const ScreenContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+`;
+
+export const ScreenTitle = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #222;
+`;
