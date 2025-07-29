12f76b69e76cde5da08020a00d6b0b52db421b23
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-07-29 13:35:07 +0900

    [style] 상단 제목 제거

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -6,7 +6,6 @@ export const options = { headerShown: false };
 export default function CalendarScreen() {
   return (
     <S.ScreenContainer>
-      <S.ScreenTitle>캘린더 페이지</S.ScreenTitle>
       <MonthCalendar />
     </S.ScreenContainer>
   );
