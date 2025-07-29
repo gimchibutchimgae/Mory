cfdb69b731b7288d2bf031d1a5859e7165bd2a4a
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-07-29 17:54:16 +0900

    [style] 캘린더 아이콘 svg 로 수정

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
+import { IconSvg } from '@/components/ui/IconSvg';
 import * as S from '@/components/ui/StyledScreen';
 import { useRouter } from 'expo-router';
-import { Image, TouchableOpacity } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 
 export const options = { headerShown: false };
 
@@ -11,10 +12,13 @@ export default function HomeScreen() {
     <S.ScreenContainer>
       <S.ScreenTitle>홈</S.ScreenTitle>
       <TouchableOpacity onPress={() => router.push('/(calendar)/calendar')}>
-        <Image
-          source={require('@/assets/icons/calendar.png')}
-          style={{ width: 48, height: 48, marginTop: 24 }}
-        />
+        <View style={{ marginTop: 24 }}>
+          <IconSvg 
+            name="calendar" 
+            size={48} 
+            color="#FFFFFF"
+          />
+        </View>
       </TouchableOpacity>
     </S.ScreenContainer>
   );
