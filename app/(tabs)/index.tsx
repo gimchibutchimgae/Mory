8ce9310bc88ddbfd17ca914c8821e9e271a4c419
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-08-01 16:13:36 +0900

    [fix] 파일 경로 수정 및 캘린더 페이지 이동 버튼 삭제

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,7 @@
-import WeekCalendar from '@/components/calendar/WeekCalendar';
-import { IconSvg } from '@/components/ui/IconSvg';
+import WeekCalendar from '@/components/calendar/weekCalendar/WeekCalendar';
 import * as S from '@/components/ui/StyledScreen';
 import { useRouter } from 'expo-router';
-import { ScrollView, TouchableOpacity, View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const options = { headerShown: false };
@@ -14,21 +13,9 @@ export default function HomeScreen() {
     <SafeAreaView style={{ flex: 1, backgroundColor: '#14213d' }}>
       <ScrollView style={{ flex: 1, backgroundColor: '#14213d' }}>
         <S.ScreenContainer>
-          
-          {/* 주간 캘린더 테스트 */}
           <View>
             <WeekCalendar useCurrentWeek={true} />
           </View>
-          
-          <TouchableOpacity onPress={() => router.push('/(calendar)/calendar')}>
-            <View style={{ marginTop: 24 }}>
-              <IconSvg 
-                name="calendar" 
-                size={27} 
-                color="#FFFFFF"
-              />
-            </View>
-          </TouchableOpacity>
         </S.ScreenContainer>
       </ScrollView>
     </SafeAreaView>
