4027913cc89ef5ea5ee80c9543b535c942fbe9ad
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-08-01 15:26:00 +0900

    [style] 상단 흰색바에 가려짐 문제 해결

--- components/ui/StyledScreen.tsx
+++ components/ui/StyledScreen.tsx
@@ -3,8 +3,9 @@ import styled from 'styled-components/native';
 export const ScreenContainer = styled.View`
   flex: 1;
   align-items: center;
-  justify-content: center;
-  background-color: #003366;
+  justify-content: flex-start;
+  background-color: #14213d;
+  padding-top: 20px;
 `;
 
 export const ScreenTitle = styled.Text`
