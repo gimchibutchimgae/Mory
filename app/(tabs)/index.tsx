c7799f8cb861479b4c50c9a6f13dd914fc72680c
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-08-01 15:25:41 +0900

    [feat] 홈 페이지 주간 캘린더 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,9 @@
+import WeekCalendar from '@/components/calendar/WeekCalendar';
 import { IconSvg } from '@/components/ui/IconSvg';
 import * as S from '@/components/ui/StyledScreen';
 import { useRouter } from 'expo-router';
-import { TouchableOpacity, View } from 'react-native';
+import { ScrollView, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const options = { headerShown: false };
 
@@ -9,17 +11,26 @@ export default function HomeScreen() {
   const router = useRouter();
 
   return (
-    <S.ScreenContainer>
-      <S.ScreenTitle>홈</S.ScreenTitle>
-      <TouchableOpacity onPress={() => router.push('/(calendar)/calendar')}>
-        <View style={{ marginTop: 24 }}>
-          <IconSvg 
-            name="calendar" 
-            size={48} 
-            color="#FFFFFF"
-          />
-        </View>
-      </TouchableOpacity>
-    </S.ScreenContainer>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#14213d' }}>
+      <ScrollView style={{ flex: 1, backgroundColor: '#14213d' }}>
+        <S.ScreenContainer>
+          
+          {/* 주간 캘린더 테스트 */}
+          <View>
+            <WeekCalendar useCurrentWeek={true} />
+          </View>
+          
+          <TouchableOpacity onPress={() => router.push('/(calendar)/calendar')}>
+            <View style={{ marginTop: 24 }}>
+              <IconSvg 
+                name="calendar" 
+                size={27} 
+                color="#FFFFFF"
+              />
+            </View>
+          </TouchableOpacity>
+        </S.ScreenContainer>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
