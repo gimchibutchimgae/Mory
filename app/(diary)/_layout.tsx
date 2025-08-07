608d661219197a8156512006c66cc8debde9f740
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-08-07 17:39:33 +0900

    [feat] 대기 화면 로직 추가

--- app/(diary)/_layout.tsx
+++ app/(diary)/_layout.tsx
@@ -4,6 +4,8 @@ export default function DiaryLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="write" />
+      <Stack.Screen name="waiting" />
+      <Stack.Screen name="analysis-result" />
     </Stack>
   );
 }
